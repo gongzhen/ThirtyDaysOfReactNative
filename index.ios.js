d21ebb97fb2d3d519d6afb23f10756fe9917b78d
gongzhen <gongzhengz@gmail.com>
2017-02-19 21:47:25 -0800

    Added header, footer, sectionHeader. Created datasource for rows, headers.

--- index.ios.js
+++ index.ios.js
@@ -16,16 +16,59 @@ import {
 } from 'react-native';
 
 import Row from './Row';
+import Header from './Header';
+import Footer from './Footer'
+import SectionHeader from './SectionHeader';
 import data from './demoData'
 
 export default class ThirtyDaysOfReactNative extends Component {
   constructor(props) {
     super(props);
 
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    const getSectionData = (dataBlob, sectionId) => dataBlob[sectionId];
+    const getRowData = (dataBlob, sectionId, rowId) => dataBlob[`${rowId}`];
+
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2,
+      sectionHeaderHasChanged: (s1, s2) => s1 !== s2,
+      getSectionData,
+      getRowData,
+    });
+    
+    // http://stackoverflow.com/questions/33798717/javascript-es6-const-with-curly-braces
+    const {dataBlob, sectionIds, rowIds} = this.formatData(data);
+
     this.state = {
-      dataSource: ds.cloneWithRows(data),
+      dataSource: ds.cloneWithRowsAndSections(dataBlob, sectionIds, rowIds),
     };
+  }
+
+  formatData(data) {
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+
+    const dataBlob = {};
+    const sectionIds = [];
+    const rowIds = [];
+
+    for (let sectionId = 0; sectionId < alphabet.length; sectionId++ ) {
+      const currentChar = alphabet[sectionId];
+      const users = data.filter((user) => user.name.first.toUpperCase().indexOf(currentChar) === 0);
+      
+      if (users.length > 0) {
+        
+        sectionIds.push(sectionId);
+        dataBlob[sectionId] = { character: currentChar };
+
+        rowIds.push([]);
+        for (let i = 0; i < users.length; i++) {
+          const rowId = `${sectionId}:${i}`;          
+          rowIds[rowIds.length - 1].push(rowId);
+
+          dataBlob[rowId] = users[i];
+        }
+      }
+    }
+    return {dataBlob, sectionIds, rowIds};
   }
 
   render() {
@@ -33,7 +76,11 @@ export default class ThirtyDaysOfReactNative extends Component {
       <ListView 
       style = {styles.container} 
       dataSource={this.state.dataSource} 
-      renderRow={(data) => <Row {...data} />}  />
+      renderRow={(data) => <Row {...data} />} 
+      renderSeparator={(sectionId, rowId) => <View key={rowId} style={styles.separator} />}
+      renderHeader={() => <Header />}
+      renderFooter={() => <Footer />}
+      renderSectionHeader = {(sectionData) => <SectionHeader {...sectionData} />} />
     );
   }
 }
@@ -43,15 +90,10 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: 20
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  separator: {
+    flex: 1,
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: '#8E8E8E',
   },
 });
 
